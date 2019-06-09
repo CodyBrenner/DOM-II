f747344f79c4fcc8fcb89fe0b8ea933056005aeb
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-06-09 12:16:47 -0700

    First Upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+// MOUSE OVER
+
+let buttonChange = document.querySelector(".btn");
+
+buttonChange.addEventListener("mouseover", function (event){
+        event.target.style.color = "red";
+});
+let buttonChangeTwo = document.querySelector("#btn2");
+
+buttonChangeTwo.addEventListener("mouseover", function (event){
+        event.target.style.color = "yellow";
+});
+let buttonChangeThree = document.querySelector("#btn3");
+
+buttonChangeThree.addEventListener("mouseover", function (event){
+        event.target.style.color = "purple";
+});
+
+window.addEventListener('keypress', () => {
+    alert("The Keyboard is Not active on this web page")
+})
+
+const card = document.querySelector('.image-mainOne');
+
+card.addEventListener('dblclick', function (e) {
+  card.classList.toggle('large');
+});
+
